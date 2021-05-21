4d4fafa28d32f433dde3e5ffd694ed7549f89d85
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-21 14:59:12 -0500

    Add Orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,43 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @orders = Order.find(params[:id])
+    render json: @orders
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      render json: order, status: :created
+    else
+      render json: order.errors, status: :bad_request
+    end
+    # TODO: handle creation of order_product related record
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      render json: order, status: :update
+    else
+      render json: order.errors, status: :bad_request
+    end
+    # TODO: handle update of order_product related record if necessary
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    render status: :no_content
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:paid_at, :status, :total_price, :user_address_id, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :offers, only: %i[index show create update destroy]
   resources :categories, only: %i[index show create update destroy]
   resources :products, only: %i[index show create update destroy]
+  resources :orders, only: %i[index show create update destroy]
 end
