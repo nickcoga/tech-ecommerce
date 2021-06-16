d6d97d6ce54d686a0e2a41eea02dcb18ab1252c8
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-16 17:43:31 -0500

    Update Component Banner Styles
    Co-authored-by: Ken Vega <kenvega@users.noreply.github.com>

--- front/src/shared/Banner.js
+++ front/src/shared/Banner.js
@@ -1,14 +1,22 @@
+import React from "react";
 import Carousel from "./Carousel/Carousel";
 import FlexContainer from "./UI/FlexContainer";
 
+const Offer = (
+  <div className="mb-2 w-full">
+    <img
+      className="object-cover h-10vh lg:h-auto mx-auto"
+      src="https://i.linio.com/cms/0fce0aae-cccd-11eb-a091-ee2255d3cf9f.webp"
+      alt="An offer"
+    />
+  </div>
+);
+
 export default function Banner() {
   return (
-    <FlexContainer className="flex-col items-center">
-      <div className="mb-2">
-        <img src="https://via.placeholder.com/1180x92" alt="An offer" />
-      </div>
-
+    <div className="lg:max-w-80vw mx-auto">
+      {Offer}
       <Carousel />
-    </FlexContainer>
+    </div>
   );
 }
