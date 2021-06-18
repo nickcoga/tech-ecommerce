6d6255efa4713e5a7c1f4a94c4d0a81f94617071
Ken Vega <ken.vega.g@gmail.com>
2021-06-18 15:56:11 -0500

    Extend the new tailwind config
    Co-authored-by: Nick Correa García <nickcoga@users.noreply.github.com>

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,8 +5,30 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "5vh": "5vh",
+        "10vh": "10vh",
+        "15vh": "15vh",
+        "20vh": "20vh",
+        "25vh": "25vh",
+      },
+      width: {
+        "5vw": "5vw",
+        "10vw": "10vw",
+        "15vw": "15vw",
+        "20vw": "20vw",
+        "25vw": "25vw",
+      },
+      maxWidth: {
+        "70vw": "70vw",
+        "75vw": "75vw",
+        "80vw": "80vw",
+        "85vw": "85vw",
+        "90vw": "90vw",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
