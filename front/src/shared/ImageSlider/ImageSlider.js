b7d080bc5b0d98a3c098cc11e6d108746821ae4d
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-22 22:54:58 -0500

    Refactor ImageSlider
    Co-authored-by: Ken Vega <kenvega@users.noreply.github.com>

--- front/src/shared/ImageSlider/ImageSlider.js
+++ front/src/shared/ImageSlider/ImageSlider.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { imageLinks } from "../../constants/hotCategories";
 import FlexContainer from "./../UI/FlexContainer";
 import "./styles.css";
 
-const links = Object.entries(imageLinks);
-
-export default function ImageSlider() {
+export default function ImageSlider({ children, className = "" }) {
   return (
-    <FlexContainer className="mt-2 gap-1 overflow-x-auto lg:justify-between">
-      {links.map(([key, url], index) => {
-        return (
-          <div key={key} className="flex-none">
-            <img src={url} alt={`hot category ${index}`}></img>
-          </div>
-        );
-      })}
+    <FlexContainer
+      className={`overflow-x-auto lg:justify-between ${className}`}
+    >
+      {children}
     </FlexContainer>
   );
 }
