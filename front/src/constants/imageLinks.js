3afe58f0483a6ef5a5fb1d058adf85701bebe3c0
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-22 22:50:26 -0500

    Update imagesLinks
    Co-authored-by: Ken Vega <kenvega@users.noreply.github.com>

---
+++ front/src/constants/imageLinks.js
@@ -0,0 +1,22 @@
+// TODO: hot category images should come from CDN or backend later
+export const categoriesLinks = {
+  category1: "https://placeimg.com/190/91/tech?random=1",
+  category2: "https://placeimg.com/190/91/tech?random=2",
+  category3: "https://placeimg.com/190/91/tech?random=3",
+  category4: "https://placeimg.com/190/91/tech?random=4",
+  category5: "https://placeimg.com/190/91/tech?random=5",
+  category6: "https://placeimg.com/190/91/tech?random=6",
+};
+
+export const productsLinks = {
+  product1: "https://placeimg.com/199/199/tech?random=1",
+  product2: "https://placeimg.com/199/199/tech?random=2",
+  product3: "https://placeimg.com/199/199/tech?random=3",
+  product4: "https://placeimg.com/199/199/tech?random=4",
+  product5: "https://placeimg.com/199/199/tech?random=5",
+  product6: "https://placeimg.com/199/199/tech?random=6",
+  product7: "https://placeimg.com/199/199/tech?random=7",
+  product8: "https://placeimg.com/199/199/tech?random=8",
+  product9: "https://placeimg.com/199/199/tech?random=9",
+  product10: "https://placeimg.com/199/199/tech?random=10",
+};
