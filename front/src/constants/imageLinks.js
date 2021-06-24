6dc2bf2a625187d0d482c6ecf7177c1065e34f57
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-24 16:05:09 -0500

    Update Constants
    Co-authored-by: Ken Vega <kenvega@users.noreply.github.com>

--- front/src/constants/imageLinks.js
+++ front/src/constants/imageLinks.js
@@ -7,16 +7,3 @@ export const categoriesLinks = {
   category5: "https://placeimg.com/190/91/tech?random=5",
   category6: "https://placeimg.com/190/91/tech?random=6",
 };
-
-export const productsLinks = {
-  product1: "https://placeimg.com/199/199/tech?random=1",
-  product2: "https://placeimg.com/199/199/tech?random=2",
-  product3: "https://placeimg.com/199/199/tech?random=3",
-  product4: "https://placeimg.com/199/199/tech?random=4",
-  product5: "https://placeimg.com/199/199/tech?random=5",
-  product6: "https://placeimg.com/199/199/tech?random=6",
-  product7: "https://placeimg.com/199/199/tech?random=7",
-  product8: "https://placeimg.com/199/199/tech?random=8",
-  product9: "https://placeimg.com/199/199/tech?random=9",
-  product10: "https://placeimg.com/199/199/tech?random=10",
-};
