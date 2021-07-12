28430c191112d722aa5ede4ecfc4e49c59e89e2b
Ken Vega <ken.vega.g@gmail.com>
2021-07-12 13:00:15 -0500

    Update Carousel
    Co-authored-by: Nick Correa García <nickcoga@users.noreply.github.com>

---
+++ front/src/constants/imagesCarosuel.js
@@ -0,0 +1,6 @@
+export const imagesCarosuel = [
+  { id: 1, url: "https://placeimg.com/1403/325/tech" },
+  { id: 2, url: "https://placeimg.com/1403/325/nature" },
+  { id: 3, url: "https://placeimg.com/1403/325/people" },
+  { id: 4, url: "https://placeimg.com/1403/325/animals" },
+];
