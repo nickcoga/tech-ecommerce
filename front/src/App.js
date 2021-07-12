59cfaabc59b9fe0101beda9feeef15776958c2b9
Ken Vega <ken.vega.g@gmail.com>
2021-07-12 13:01:24 -0500

    Add routes to App component
    Co-authored-by: Nick Correa García <nickcoga@users.noreply.github.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './styles/app.css'
 import LandPage from "./page/LandPage";
+import Cart from "./page/Cart";
+import PartnershipDiscounts from "./page/PartnershipDiscounts";
+import SpecialDiscounts from "./page/SpecialDiscounts";
+import ProductDetails from "./page/ProductDetails";
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandPage} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/partnership-discounts" component={PartnershipDiscounts} />
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/special-offers/:id" component={SpecialDiscounts} />
       </Switch>
     </Router>
   );
