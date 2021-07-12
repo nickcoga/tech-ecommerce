5044e98fce6d4e45dc08e0cac1ac5a566a65534f
Ken Vega <ken.vega.g@gmail.com>
2021-07-12 12:59:37 -0500

    Create new pages to be routed
    Co-authored-by: Nick Correa García <nickcoga@users.noreply.github.com>

---
+++ front/src/page/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../shared/Header";
+import MainContainer from "../shared/UI/MainContainer";
+import SectionContainer from "../shared/UI/SectionContainer";
+
+export default function Cart() {
+  return (
+    <MainContainer>
+      <Header />
+      <SectionContainer>Cart Page</SectionContainer>
+    </MainContainer>
+  );
+}
---
+++ front/src/page/PartnershipDiscounts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../shared/Header";
+import MainContainer from "../shared/UI/MainContainer";
+import SectionContainer from "../shared/UI/SectionContainer";
+
+export default function PartnershipDiscounts() {
+  return (
+    <MainContainer>
+      <Header />
+      <SectionContainer>
+        <div>Partnership discounts</div>
+      </SectionContainer>
+    </MainContainer>
+  );
+}
